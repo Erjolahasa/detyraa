b2edc8ac7017f4e1572c3ec05dbf3acbdb501337
Erjolahasa <70211786+Erjolahasa@users.noreply.github.com>
2020-09-07 15:29:55 +0200

    Add files via upload

---
+++ User.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace KlinikeMjekesore.Models.extended
+{
+    [MetadataType(typeof(UserMetadata))]
+    public class User
+    {
+        public string ConfirmPassword { get; set; }
+
+    }
+    public class UserMetadata
+    {
+        [Display(Name ="First Name")]
+        [Required(AllowEmptyStrings =false,ErrorMessage ="First Name required")]
+        public string FirstName { get; set; }
+
+        [Display(Name = "Last Name")]
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Last Name required")]
+        public string LastName { get; set; }
+
+        [Display(Name = "Email")]
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Email Required")]
+        [DataType(DataType.EmailAddress)]
+        public string Email { get; set; }
+
+        [Display(Name ="Date of birth")]
+        [DataType(DataType.Date)]
+        [DisplayFormat(ApplyFormatInEditMode =true,DataFormatString ="{0:MM/dd/yyyy")]
+        public DateTime Dateofbirth { get; set; }
+
+        [Required(AllowEmptyStrings =false,ErrorMessage ="Password is required")]
+        [DataType(DataType.Password)]
+        [MinLength(6,ErrorMessage ="Minimum 6 character required")]
+        public string Password { get; set; }
+
+        [Display(Name = "Confirm Password")]
+        [DataType(DataType.Password)]
+        [Compare("Password", ErrorMessage = "Confirm Password and Password do not match")]
+        public string ConfirmPassword{ get; set; }
+    }
+}
